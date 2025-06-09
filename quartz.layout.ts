a2a1acc26ae4800605da53d06c95df5e0399b3a1
ZxNashx <axylschulz0@gmail.com>
2025-06-09 16:01:16 -0600

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -41,7 +41,7 @@ export const defaultContentPageLayout: PageLayout = {
   ],
   right: [
     Component.Graph(),
-    Component.DesktopOnly(Component.TableOfContents()),
+    //Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
 }
